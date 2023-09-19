f04cc999b09f260a4953498afffbcaf25d5e0faf
dhanjyoti <dhanjyotir20@gmail.com>
2023-09-19 14:58:43 +0530

    higher order component

---
+++ basic7_high_order_component/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MovieList from './MovieList';
+import MovieAnalytics from './MovieAnalytics';
+
+function App() {
+  return (
+    <>
+      <MovieList />
+      <MovieAnalytics />
+    </>
+  );
+}
+
+export default App;
