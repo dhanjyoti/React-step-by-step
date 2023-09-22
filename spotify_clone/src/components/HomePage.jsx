fb1937e50d3a11d025a82b1f18aeeb3ffa86c8d6
dhanjyoti <dhanjyotir20@gmail.com>
2023-09-22 16:14:23 +0530

    spotify clone home component created

---
+++ spotify_clone/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import './componentStyles/homepage.css';
+
+const HomePage = () => {
+  return (
+    <div className='HomeComponent'>
+        <div>Logo</div>
+        <h1>Home</h1>
+        <input type='search' />
+    </div>
+  )
+}
+
+export default HomePage
