e959459b780a56eed575b9982c5be620157eb3ce
dhanjyoti <dhanjyotir20@gmail.com>
2023-09-25 13:58:22 +0530

    change added

--- spotify_clone/src/components/HomePage.jsx
+++ spotify_clone/src/components/HomePage.jsx
@@ -1,11 +1,14 @@
+import HomeIcon from '@mui/icons-material/Home';
+import { Search } from '@mui/icons-material';
 import './componentStyles/homepage.css';
 
 const HomePage = () => {
   return (
     <div className='HomeComponent'>
         <div>Logo</div>
-        <div>Home</div>
+        <div><HomeIcon />Home</div>
         <div>Search</div>
+        <Search/>
     </div>
   )
 }
