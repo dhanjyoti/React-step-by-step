0b0932113660eae9fa866a5af09c8c403af318e7
dhanjyoti <dhanjyotir20@gmail.com>
2023-09-28 07:23:41 +0530

    login page content added

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './components/HomePage';
+import Login from './route/Login';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className="w-full h-full flex flex-col items-center">
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ spotify_clone/src/components/share/TextInput.jsx
@@ -0,0 +1,12 @@
+
+
+const TextInput = ({label, placeholder}) => {
+  return (
+    <div className="flex flex-col space-y-1 w-full">
+        <label for={label}>{label}</label>
+        <input type="text" placeholder={placeholder} className="p-3 border border-gray-400 border-solid rounded placeholder-gray-500" />
+    </div>
+  )
+}
+
+export default TextInput
