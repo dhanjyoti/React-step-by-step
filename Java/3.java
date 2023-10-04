bc6468871e445236c557310e6f70e1bc1f2eb451
dhanjyoti <dhanjyotir20@gmail.com>
2023-10-04 15:06:31 +0530

    swap number program

--- Java/3.java
+++ Java/3.java
@@ -1,1 +1,19 @@
-// Java
+// Java Program to Swap Two Numberes
+
+import java.util.Scanner;
+
+public class SwapNumber{
+    static void swapNumberValues(int m, int n){
+        // Swapping the values
+        int temp = m;
+        m = n;
+        n = temp;
+        System.out.println("Value of m is: " + m +" and Value of n is: " + n);
+    }
+    public static void main(String... args){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        swapNumberValues(m, n);
+    }
+}
