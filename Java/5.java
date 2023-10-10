4d8a4b7533194b25aea5a7838561eca4e5e8891f
dhanjyoti <dhanjyotir20@gmail.com>
2023-10-10 10:58:09 +0530

    files added

---
+++ Java/5.java
@@ -0,0 +1,16 @@
+// Java Program to Check if Given Integer is Odd or Even
+
+import java.io.*;
+import java.util.*;
+
+class OddEven {
+    public static void main(String... args){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if(number % 2 == 0){
+            System.out.print("Even Number");
+        } else {
+            System.out.print("Odd Number");
+        }
+    }
+}
