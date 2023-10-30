b81c8af3efc52697b690901a2246c31122630d7d
dhanjyoti <dhanjyotir20@gmail.com>
2023-10-30 14:03:58 +0530

    update

---
+++ Javascript/Practice/8.js
@@ -0,0 +1,53 @@
+// const student = {
+//     name: "donkey",
+//     age: 101,
+//     standard: "1st",
+//     school:{
+//         place:"Makkhanpur",
+//         state:"Guj",
+        
+//     }
+// }
+// const stu = {details:()=> {return student.name + " is of age " + student.age}};
+// // const good = stu.details();
+// let goode = stu.details.bind(this);
+// console.log(goode());
+
+// class x {
+
+//     y = 10;
+//     z = null
+//     constructor(){
+
+//         console.log(this.y);
+
+//         this.z = ()=>{ 
+//             console.log("hello")
+//         }
+       
+//     }
+
+//     abc = ()=>{
+
+        
+//         window.addEventListener("click", function () {
+//             // console.log(this.z());
+//             this.z()
+//         }.bind(this)) 
+//     }
+
+// }
+
+// let c = new x()
+// c.z()
+// c.abc()
+
+
+// const b = ()=>{
+//     let a = 10;
+//     let b = 20;
+//     let c = a + b;
+//     console.log(c);
+// }
+
+// b()
