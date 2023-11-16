04bfa050ee27e843c8a9dcf0aa4d021376d929be
dhanjyoti <dhanjyotir20@gmail.com>
2023-11-16 20:17:21 +0530

    timer added

--- Javascript/form_interview/script.js
+++ Javascript/form_interview/script.js
@@ -1,27 +1,40 @@
-document.getElementById("inputForm").addEventListener("submit", function(e){
-    e.preventDefault();
+document.getElementById("inputForm").addEventListener("submit", function(event){
+    event.preventDefault();
 
     const input1Value = document.getElementById("input1").value;
     const input2Value = document.getElementById("input2").value;
     
     const outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = `
-                        <p>${input1Value}</p>
-                        <p>${input2Value}</p>`;
+    outputDiv.innerHTML =  `<p>Input 1: ${input1Value}</p>
+                            <p>Input 2: ${input2Value}</p>`;                        
 
     let seconds = 0;
-    const timer = document.getElementById("time");
+    const hoursDisplay = document.getElementById("hours");
+    const minutesDisplay = document.getElementById("minutes");
+    const secondsDisplay = document.getElementById("seconds");
+
     const interval = setInterval(function(){
         seconds++;
-        timer.textContent = seconds;
-    }, 1000);
+        const hours = Math.floor(seconds/3600);
+        const minutes = Math.floor((seconds%3600)/60);
+        const remainingSeconds = seconds % 60;
 
-    setTimeout(function(){
-        clearInterval(interval);
-    }, 10000);
+        hoursDisplay.textContent = hours < 10 ? `0${hours}` : hours;
+        minutesDisplay.textContent = minutes < 10 ? `0${minutes}` : minutes;
+        secondsDisplay.textContent = remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds;
+    }, 1000);
 });
 
 
+    // const timer = document.getElementById("time");
+    // const interval = setInterval(function(){
+    //     seconds++;
+    //     timer.textContent = seconds;
+    // }, 1000);
+
+    // setTimeout(function(){
+    //     clearInterval(interval);
+    // }, 10000);
 
     // console.log(input1Value);
     // console.log(input2Value);
