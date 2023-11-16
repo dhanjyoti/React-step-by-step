9232aaaa9828d26be36a62c74344401821c20233
dhanjyoti <dhanjyotir20@gmail.com>
2023-11-16 11:03:11 +0530

    JS engine

--- Javascript/Practice/7.js
+++ Javascript/Practice/7.js
@@ -1,3 +1,27 @@
-function dhan(){
-    document.getElementById('demo').innerHTML=Date();
-}
+// function dhan(){
+//     document.getElementById('demo').innerHTML=Date();
+// }
+
+
+// *** Math.random ***
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+// *** setTimeout for callback ***
+// document.getElementById('wow').addEventListener("click", setTimeout(function xyz(){
+//     document.write("button clicked", ++count);
+// }, 3000));
+
+// *** Number of Counts ***
+
+// function attachEventListener(){
+//     let count = 0;
+//     document.getElementById('wow').addEventListener("click", function xyz(){
+//         console.log("button clicked", ++count);
+//     });
+// }
+// attachEventListener();
