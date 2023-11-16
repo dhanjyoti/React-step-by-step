b299d54a2427bc5a6e3c11c3479073fc596bdbf1
dhanjyoti <dhanjyotir20@gmail.com>
2023-11-16 11:02:18 +0530

    empty

--- Javascript/Practice/JS_Indebt/id1.js
+++ Javascript/Practice/JS_Indebt/id1.js
@@ -1,41 +1,5 @@
-// function add(a,b,c){
-//     return a+b+c;
 
-// }
-
-// let result = add(1,2,3);
-// console.log(result)
-
-// curring function
-
-function Addition(a) {
-    return function (b) {
-      return function (c) {
-        return a + b + c;
-      };
-    };
-  }
-  
-  // let result = Addition(4);
-  // console.log(result);
-  // let data = result(3);
-  // console.log(data);
-  // let data1 = data(4);
-  // console.log(data1);
-  
-  let result = Addition(4)(3)(4);
-  console.log(result);
-  
-  userData = {
-    name: "Nidhi",
-    age: "24",
-    Product: "lakme Kajal",
-  };
-  function userInfo(obj) {
-    return function (userInfo) {
-      return obj[userInfo];
-    };
-  }
-  
-  let res = userInfo(userData)("name");
-  console.log(res);
+    let count = 0;
+    document.getElementById('wow').addEventListener("click", function xyz(){
+        console.log("button clicked", ++count);
+    });
