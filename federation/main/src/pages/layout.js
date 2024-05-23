aecf38fe9d63eb4bd85989a20c985537d84d17a0
dhanjyoti <dhanjyotir20@gmail.com>
2024-05-24 01:01:04 +0530

    side bar modified

--- federation/main/src/pages/layout.js
+++ federation/main/src/pages/layout.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-col gap-5 w-[250px] px-10 pt-16">
-        <Link href={"/weather"} className="bg-blue-600 w-fit px-4 py-1 rounded-md">Weather</Link>
-        <Link href={"/simple-interest"} className="bg-blue-600 w-fit px-4 py-1 rounded-md">Simple Interest</Link>
-        <Link href={"/show-input"} className="bg-blue-600 w-fit px-4 py-1 rounded-md">Show Input</Link>
+    <div className="flex flex-row h-[100vw]">
+      <div className="flex flex-col gap-5 w-[250px] px-10 pt-16 bg-blue-200 h-auto">
+        <Link href={"/weather"} className="bg-orange-500 w-fit px-4 py-1 rounded-md">Weather</Link>
+        <Link href={"/simple-interest"} className="bg-orange-500 w-fit px-4 py-1 rounded-md">Simple Interest</Link>
+        <Link href={"/show-input"} className="bg-orange-500 w-fit px-4 py-1 rounded-md">Show Input</Link>
       </div>
       <div className="flex-1">{children}</div>
     </div>
